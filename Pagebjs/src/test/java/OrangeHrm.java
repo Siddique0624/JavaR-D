af6c1a5d65b74910149cb7f0fb53b85d9ec4f9ae
Siddique0624 <siddiqueumar42+git@gmail.com>
2023-10-30 11:36:59 +0530

    This is a POM based automation project

---
+++ Pagebjs/src/test/java/OrangeHrm.java
@@ -0,0 +1,33 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrangeHrm {
+
+	
+	@Test
+	public void postivcredentials() {
+     	WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		Home h=new Home(driver);
+		h.typeusername();
+		h.typepasswor();
+		h.Clickonllogin();
+		
+
+		Login l=new Login(driver);
+		l.clickonmarket();
+	 
+		
+		Welcome w=new  Welcome(driver);
+		w.clickonrequest();
+}
+}
