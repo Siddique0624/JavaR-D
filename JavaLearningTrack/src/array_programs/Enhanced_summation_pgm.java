fd635b9126cc1ea4ac37dc0670ba73dbadec436b
Siddique0624 <siddiqueumar42+git@gmail.com>
2023-11-27 20:58:35 +0530

    Sum of array Java program

---
+++ JavaLearningTrack/src/array_programs/Enhanced_summation_pgm.java
@@ -0,0 +1,15 @@
+package array_programs;
+
+public class Enhanced_summation_pgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int numbers[]= {1,2,3,4,5,6};
+     int sum=0;
+     for (int data:numbers) {
+       sum+=data;    	 
+     }
+     System.out.println("Sum of the array numbers is "+sum);
+	}
+
+}
