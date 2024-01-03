9e07b644cecb50671f5c6de33775e06e6487a25b
Siddique0624 <siddiqueumar42+git@gmail.com>
2024-01-03 12:07:18 +0530

    Reversing the letters in a word individally in a given sentance

---
+++ JavaLearningTrack/src/string_pgms/Reverse_onlywords_inString.java
@@ -0,0 +1,28 @@
+package string_pgms;
+import java.util.Scanner;
+
+public class Reverse_onlywords_inString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Original string : ");
+ 
+        String originalStr = scanner.nextLine();
+       // scanner.close();
+ 
+        String words[] = originalStr.split("\\s");
+        String reversedString = "";
+ 
+        for (int i = 0; i < words.length; i++) 
+        {
+            String word = words[i];
+            String reverseWord = "";
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reversedString = reversedString + reverseWord + " ";
+        }
+ 
+        // Displaying the string after reverse
+        System.out.println("Reversed string : " + reversedString);
+    }
+}
