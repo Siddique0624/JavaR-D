f9a399b5fd1f713a92e02f060472830a628ae0f1
Siddique0624 <siddiqueumar42+git@gmail.com>
2024-03-06 14:07:02 +0530

    Made require changes for my name java pgmd

--- JavaLearningTrack/src/pattern_pgm/Alphabets/My_Name.java
+++ JavaLearningTrack/src/pattern_pgm/Alphabets/My_Name.java
@@ -1,3 +1,4 @@
+package pattern_pgm.Alphabets;
 import java.util.Scanner;
 class My_Name 
 {
--- JavaLearningTrack/src/pattern_pgm/Alphabets/Siddique_star_pattern.java
+++ JavaLearningTrack/src/pattern_pgm/Alphabets/Siddique_star_pattern.java
@@ -1,3 +1,4 @@
+package pattern_pgm.Alphabets;
 import java.util.Scanner;
 class Siddique_star_pattern 
 {
