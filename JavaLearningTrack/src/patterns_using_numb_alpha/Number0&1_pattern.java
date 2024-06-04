c28ee1b95b96bc6300cfce61df17613e21e78d0c
Siddique0624 <143868624+Siddique0624@users.noreply.github.com>
2024-06-04 07:10:35 +0530

    java pattern pgm using 0 & 1

---
+++ JavaLearningTrack/src/patterns_using_numb_alpha/Number0&1_pattern.java
@@ -0,0 +1,27 @@
+package patterns_using_numb_alpha;
+import java.util.Scanner;
+class Number_pattern1234 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter thenumber of rows");
+	Scanner scan=new Scanner(System.in);
+    int n=scan.nextInt();
+		for(int i=0;i<=n;i++){
+			for(int j=0;j<=n;j++){
+			if( j%2!=0){
+				System.out.print("1");
+			}
+			else{
+			System.out.print("0"); 
+				
+			}
+		}
+			 
+			System.out.println();
+		
+			
+			}
+		}
+}
+ 
